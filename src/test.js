370be63711354b6608a9fd8d5051bb1b6999da7f
fatihkarasoglu <f.karasoglu.01@gmail.com>
2023-05-03 01:50:44 +0300

    commit-3

--- src/test.js
+++ src/test.js
@@ -1,31 +1,46 @@
-import React from 'react'
-import { useEffect } from 'react'
+import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 export default function Test() {
-    const [count, setCount] = useState(0);
+    const [postId, setPostId] = useState(1);
+    const [post, setPost] = useState(false);
 
     useEffect(() => {
-        console.log('component ilk yüklendiğinde çalışır')
+        console.log('component ilk yüklendiğinde çalışır');
+
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(json => console.log(json))
+        
+        let interval = setInterval(() => console.log('İnterval çalıştı!'), 1000);
+
         return() => {
-            console.log('component destroyed')
+            console.log('component destroyed');
+            clearInterval(interval);
         }
-    });
-
-    useEffect(() => {
-        console.log('component render oldu')
     }, []);
 
     useEffect(() => {
-        console.log('Count değeri değişti!', count);
-    }, [count]);
+        console.log('postId değeri değişti = ', postId);
+    }, [postId]);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+            .then(res => res.json())
+            .then(data => setPost(data))
+    }, [postId])
+
+    // useEffect(() => {
+    //     console.log('component render oldu');
+    // });
 
   return (
     <div>
-        <h3>{count}</h3>
-        <button onClick={() => setCount(count => count + 1)}>Artır</button>
+        <h3>{postId}</h3>
+        {post && JSON.stringify(post)}
         <hr/>
-        test
+        <button onClick={() => setPostId(postId => postId + 1)}>Diğer Seçim</button>
     </div>
-  )
+  );
 }
