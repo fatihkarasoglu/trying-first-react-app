f08e02ebdf3b2413dd7a41dbc31496184d12e38b
fatihkarasoglu <f.karasoglu.01@gmail.com>
2023-05-05 02:22:33 +0300

    commit-3.2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
+// import { useState } from 'react';
+// import { useRef } from 'react';
 import { useState } from 'react';
-import { useRef } from 'react';
 import './App.css';
-import Test from './test';
+// import Test from './test';
 
 function App() {
-  const [show, setShow] = useState(false);
-
-  const inputRef = useRef();
-  const odakInput = () => {
-      console.log(inputRef.current);
-      inputRef.current.focus();
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState();
+  const submitHandle = e => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo('');
   }
+
+  // const [show, setShow] = useState(false);
+
+  // const inputRef = useRef();
+  // const odakInput = () => {
+  //     console.log(inputRef.current);
+  //     inputRef.current.focus();
+  // }
 
   return (
     <div className="App">
-      {/* <h3> {process.env.NODE_ENV} </h3> */}
+      <h1>Todos</h1>
+      <form onSubmit={submitHandle}>
+        <input type='text' value={todo} onChange={e => setTodo(e.target.value)}></input>
+        <button disabled={!todo} type='submit'>Ekle</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+          </li>
+        ))}
+      </ul>
+
+      {/* <h3> {process.env.NODE_ENV} </h3>
       <button onClick={() => setShow(show => !show)}>
         {show ? 'Gizle' : 'Göster'}
       </button>
       {show && <Test/>}
       <hr/>
       <input type='text' ref={inputRef}></input>
-      <button onClick={odakInput}>Odakla</button>
+      <button onClick={odakInput}>Odakla</button> */}
     </div>
   );
 }
--- src/test.js
+++ src/test.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+// import { useEffect } from 'react';
+// import { useState } from 'react';
 
 export default function Test() {
-    const [postId, setPostId] = useState(1);
-    const [post, setPost] = useState(false);
+    // const [postId, setPostId] = useState(1);
+    // const [post, setPost] = useState(false);
 
-    useEffect(() => {
-        console.log('component ilk yüklendiğinde çalışır');
+    // useEffect(() => {
+    //     console.log('component ilk yüklendiğinde çalışır');
 
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
-            .then(response => response.json())
-            .then(json => console.log(json))
+    //     fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //         .then(response => response.json())
+    //         .then(json => console.log(json))
         
-        let interval = setInterval(() => console.log('İnterval çalıştı!'), 1000);
+    //     let interval = setInterval(() => console.log('İnterval çalıştı!'), 1000);
 
-        return() => {
-            console.log('component destroyed');
-            clearInterval(interval);
-        }
-    }, []);
+    //     return() => {
+    //         console.log('component destroyed');
+    //         clearInterval(interval);
+    //     }
+    // }, []);
 
-    useEffect(() => {
-        console.log('postId değeri değişti = ', postId);
-    }, [postId]);
+    // useEffect(() => {
+    //     console.log('postId değeri değişti = ', postId);
+    // }, [postId]);
 
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-            .then(res => res.json())
-            .then(data => setPost(data))
-    }, [postId])
+    // useEffect(() => {
+    //     fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    //         .then(res => res.json())
+    //         .then(data => setPost(data))
+    // }, [postId])
 
     // useEffect(() => {
     //     console.log('component render oldu');
@@ -37,10 +37,10 @@ export default function Test() {
 
   return (
     <div>
-        <h3>{postId}</h3>
+        {/* <h3>{postId}</h3>
         {post && JSON.stringify(post)}
         <hr/>
-        <button onClick={() => setPostId(postId => postId + 1)}>Diğer Seçim</button>
+        <button onClick={() => setPostId(postId => postId + 1)}>Diğer Seçim</button> */}
     </div>
   );
 }
