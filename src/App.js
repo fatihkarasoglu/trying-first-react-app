0cb8721403079b6b15e288d45ae181ce7ad94d84
fatihkarasoglu <f.karasoglu.01@gmail.com>
2023-05-05 05:12:10 +0300

    commit-3.3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
-// import { useState } from 'react';
+import { useReducer } from 'react';
 // import { useRef } from 'react';
-import { useState } from 'react';
 import './App.css';
 // import Test from './test';
 
+function reducer(state, action) {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case 'SET_TODO':
+      return{
+        ...state,
+        todo: action.value
+      }
+    case 'ADD_TODO':
+      return{
+        ...state,
+        todo: '',
+        todos: [
+          ...state.todos,
+          action.todo
+        ]
+      }
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState();
+  const [state, dispatch] = useReducer(reducer, {
+    todos: [],
+    todo: ''
+  })
+
+  // const [todos, setTodos] = useState([]);
+  // const [todo, setTodo] = useState();
   const submitHandle = e => {
     e.preventDefault();
-    setTodos([...todos, todo]);
-    setTodo('');
+    dispatch({
+      type: 'ADD_TODO',
+      todo: state.todo
+    })
+    // setTodos([...todos, todo]);
+    // setTodo('');
+  }
+  const onChange = e => {
+    dispatch({
+      type: 'SET_TODO',
+      value: e.target.value
+    })
   }
 
   // const [show, setShow] = useState(false);
@@ -25,11 +59,11 @@ function App() {
     <div className="App">
       <h1>Todos</h1>
       <form onSubmit={submitHandle}>
-        <input type='text' value={todo} onChange={e => setTodo(e.target.value)}></input>
-        <button disabled={!todo} type='submit'>Ekle</button>
+        <input type='text' value={state.todo} onChange={onChange}></input>
+        <button disabled={!state.todo} type='submit'>Ekle</button>
       </form>
       <ul>
-        {todos.map((todo, index) => (
+        {state.todos.map((todo, index) => (
           <li key={index}>
             {todo}
           </li>
